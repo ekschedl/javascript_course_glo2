cc3778c4612e1ae0471fa311a060979e9a939269
Ekaterina Schedlberger <ek.schedl@gmail.com>
2024-03-22 16:35:51 +0100

    lesson 2done

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-const title = "";
-const screens = "";
-const screenPrice = "";
-const rollback = "";
-const fullPrice = "";
-const adaptive = "";
+const title = "Проект 1";
+const screens = "Простые, Сложные, Интерактивные";
+const screenPrice = 1000;
+const rollback = Math.floor(Math.random() * 100);
+const fullPrice = 100000;
+const adaptive = true;
 
-alert("This is my first homework");
 console.log("Great! it works :-)!");
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+const screenPriceInRub = "Стоимость верстки экранов " + screenPrice + " рублей";
+const fullPriceInRub = "Стоимость разработки сайта " + fullPrice + " рублей";
+console.log('"' + screenPriceInRub + '"' + " и " + '"' + fullPriceInRub + '"');
+
+console.log(screens.toLowerCase().split(", "));
+
+const percentForAgent = fullPrice * (rollback / 100);
+console.log(percentForAgent);
