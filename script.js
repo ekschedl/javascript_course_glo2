505452f877c16d07b0e0067a50f56756d8473a5b
Ekaterina Schedlberger <ek.schedl@gmail.com>
2024-03-22 15:12:41 +0100

    plus lesson01

---
+++ script.js
@@ -0,0 +1,9 @@
+const title = "";
+const screens = "";
+const screenPrice = "";
+const rollback = "";
+const fullPrice = "";
+const adaptive = "";
+
+alert("This is my first homework");
+console.log("Great! it works :-)!");
