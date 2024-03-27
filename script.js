7fbb365aa103ce05d426a7113443e82aad6daada
Ekaterina Schedlberger <ek.schedl@gmail.com>
2024-03-27 22:04:30 +0100

    + Practice from lesson05

--- script.js
+++ script.js
@@ -1,25 +1,40 @@
 "use strict";
-let title = prompt("Как называется ваш проект?", "Калькулятор верстки");
-let screens = prompt(
-  "Какие типы экранов нужно разработать?",
-  "Простые, Сложные, Интерактивные"
-);
-let screenPrice = +prompt("Сколько будет стоить данная работа?", "15000");
-let adaptive = confirm("Нужен ли адаптив на сайте?");
+let title;
+let screens;
+let screenPrice;
+let adaptive;
 
 let rollback = 10;
 let allServicePrices;
 let fullPrice;
 let servicePercentPrice;
+let service1;
+let service2;
 
-// let service1 = prompt("Какой дополнительный тип услуги нужен?");
-// let servicePrice1 = +prompt("Сколько это будет стоить?");
-// let service2 = prompt("Какой дополнительный тип услуги нужен?");
-// let servicePrice2 = +prompt("Сколько это будет стоить?");
+const isNumber = function (num) {
+  return !isNaN(parseFloat(num)) && isFinite(num);
+};
+const asking = function () {
+  title = prompt("Как называется ваш проект?", "Калькулятор верстки");
+  screens = prompt(
+    "Какие типы экранов нужно разработать?",
+    "Простые, Сложные, Интерактивные"
+  );
+  screenPrice = prompt("Сколько будет стоить данная работа?");
 
+  while (!isNumber(screenPrice)) {
+    screenPrice = prompt("Сколько будет стоить данная работа?");
+  }
+  adaptive = confirm("Нужен ли адаптив на сайте?");
+};
 const getAllServicePrices = function () {
   let sum = 0;
   for (let i = 0; i < 2; i++) {
+    if (i === 0) {
+      service1 = prompt("Какой дополнительный тип услуги нужен?");
+    } else if (i === 1) {
+      service2 = prompt("Какой дополнительный тип услуги нужен?");
+    }
     sum += +prompt("Сколько это будет стоить?");
   }
   return sum;
@@ -52,7 +67,7 @@ const getRollbackMessage = function (price) {
     return "Что-то пошло не так";
   }
 };
-
+asking();
 allServicePrices = getAllServicePrices();
 fullPrice = getFullPrice();
 servicePercentPrice = getServicePercentPrice();
