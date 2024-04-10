25551c0428c9971cde4f90657e0b3338a9092ce0
Ekaterina Schedlberger <ek.schedl@gmail.com>
2024-04-10 13:39:48 +0200

    done

--- lesson15/script.js
+++ lesson15/script.js
@@ -1,1 +1,47 @@
 "use strict";
+
+const DomElement = function (selector, height, width, bg, fontSize, text) {
+  this.selector = selector;
+  this.height = height;
+  this.width = width;
+  this.bg = bg;
+  this.fontSize = fontSize;
+  this.text = text;
+
+  this.createElement = function () {
+    let element;
+    if (this.selector[0] === ".") {
+      element = document.createElement("div");
+      element.classList.add(this.selector.slice(1));
+    } else if (this.selector[0] === "#") {
+      element = document.createElement("p");
+      element.id = this.selector.slice(1);
+    }
+    element.textContent = this.text;
+    element.style.height = this.height + "px";
+    element.style.width = this.width + "px";
+    element.style.backgroundColor = this.bg;
+    element.style.fontSize = this.fontSize + "px";
+    document.body.appendChild(element); // Добавляем созданный элемент в тело документа
+  };
+
+  // Вызываем метод createElement при создании экземпляра объекта
+  this.createElement();
+};
+
+const elementNew = new DomElement(
+  "#test",
+  100,
+  100,
+  "yellow",
+  15,
+  "This is my text"
+);
+console.log(elementNew);
+
+// УСЛОЖНЕННОЕ  ЗАДАНИЕ: Создаем новый объект на основе класса DomElement
+
+const square = new DomElement(".square", 100, 100, "blue");
+
+square.position = "absolute";
+square.createElement();
