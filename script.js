fe2f79e8cd7f94352ffe4df9280572a398fafd9e
Ekaterina Schedlberger <ek.schedl@gmail.com>
2024-04-11 21:59:24 +0200

    done

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
-const title = "";
-const screens = "";
-const screenPrice = "";
-const rollback = "";
-const fullPrice = "";
-const adaptive = "";
+"use strict";
+// Определение класса First
+class First {
+  constructor() {}
+  methodOfFirst() {
+    // Вложенная функция hello
+    function hello() {
+      console.log("Привет я метод родителя!");
+    }
+    // Вызов вложенной функции
+    hello();
+  }
+}
 
-alert("This is my first homework");
-console.log("Great! it works :-)!");
+// Определение класса Second, который наследует от класса First
+class Second extends First {
+  constructor() {
+    super(); // Вызываем конструктор родительского класса
+  }
+
+  // Переопределяем метод methodOfFirst
+  methodOfFirst() {
+    // Вызываем метод methodOfFirst родительского класса First
+    super.methodOfFirst();
+    // Печатаем дополнительное сообщение
+    console.log("А я наследуемый метод!");
+  }
+}
+
+// Создание объекта класса Second
+let obj = new Second();
+
+// Теперь объект obj имеет доступ к методу methodOfFirst, унаследованному от класса First
+obj.methodOfFirst();
